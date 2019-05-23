35bf045760976812c552cdf748fec693807ebd3e
KonstVolkovnitsky <konst.volkovnitsky@gmail.com>
2019-05-23 16:20:32 +0300

    Solution

--- scripts.js
+++ scripts.js
@@ -1,15 +1,39 @@
-function isValidData(data, pattern) {
+window.onload = () => {
+  function isValidData(data, pattern) {
   return pattern.test(data);
 }
+
+
+document.querySelector('#submitButton').disabled = true;
+
+const changeButtonState = function() {
+  if ( 
+    ((isNameEntered && isNumberEntered) || (isCheckboxChecked && isNameEntered)
+    && isRegionSelected 
+    && isCitySeleted)) {
+    document.querySelector('#submitButton').disabled = true;
+  } else {
+    document.querySelector('#submitButton').disabled = false;
+  }
+}
+
+let isNameEntered = false;
+let isNumberEntered = false;
+let isCheckboxChecked = false;
+let isRegionSelected = false;
+let isCitySeleted = false;
 
 document.querySelector('#fullName').addEventListener('change', function(event) {
   const fullName = document.forms['mainForm'].elements['fullName'].value.trim();
   const validName = /^[А-ЯA-Z][а-яa-zА-ЯA-Z\-]{0,}\s[А-ЯA-Z][а-яa-zА-ЯA-Z\-]{1,}(\s[А-ЯA-Z][а-яa-zА-ЯA-Z\-]{1,})?$/;
   if (isValidData(fullName, validName)) {
     document.querySelector('#fullName').style.backgroundColor = "green";
+    isNameEntered = true;
   } else {
     document.querySelector('#fullName').style.backgroundColor = "red";
+    isNameEntered = false;
   }
+  changeButtonState();
 });
 
 document.querySelector("#phoneNumber").addEventListener('change', function(event) {
@@ -17,55 +41,58 @@ document.querySelector("#phoneNumber").addEventListener('change', function(event
   const validPhoneNumber = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
   if (isValidData(phoneNumber, validPhoneNumber)) {
     document.querySelector('#phoneNumber').style.backgroundColor = "green";
+    isNumberEntered = true;
   } else {
     document.querySelector('#phoneNumber').style.backgroundColor = "red";
+    isNumberEntered = false;
   }
-  
+  changeButtonState();
 });
 
 document.querySelector('#selectRegion').addEventListener('change', function(event) {
   
-  // let optionsArray = [];
-  // for (let i =0; i < document.querySelector('#selectRegion').options.length; i++) {
-  //   if (document.querySelector('#selectRegion').options[i].selected) {
-  //     optionsArray = document.querySelector('#selectRegion').options[i].value.match(/^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/g);
-  //   }
-  // }
-
-
-  const centralCities = ["Cherkasy", "Dnipro", "Kropyvnytskyi", "Poltava", "Vinnytsia", "Zhytomyr"];
-  const northernCities = ["Chernihiv", "Sumy"];
-  const easternCities = ["Donetsk", "Kharkiv", "Luhansk"];
-  const southernCities = ["Kherson", "Mykolaiv", "Odesa", "Zaporizhzhia"];
-  const westernCities = ["Chernivtsi", "Ivano-Frankivsk", "Khmelnytskyi", "Lutsk", "Lviv", "Rivne", "Ternopil", "Uzhhorod"];
-  const districtsArray = [centralCities, northernCities, easternCities, southernCities, westernCities]
+  const optionsObject = {
+    Center: ["Cherkasy", "Dnipro", "Kropyvnytskyi", "Poltava", "Vinnytsia", "Zhytomyr"],
+    North: ["Chernihiv", "Sumy"],
+    East: ["Donetsk", "Kharkiv", "Luhansk"],
+    South: ["Kherson", "Mykolaiv", "Odesa", "Zaporizhzhia"],
+    West: ["Chernivtsi", "Ivano-Frankivsk", "Khmelnytskyi", "Lutsk", "Lviv", "Rivne", "Ternopil", "Uzhhorod"]
+  };
   
-  for (let i = 0; i < document.querySelector('#selectRegion').options.length; i++) {
-    if (document.querySelector('#selectRegion').options[i].selected) {
-      for (let j = 0; j < districtsArray[i].length; i++) {
-        const option = new Option(districtsArray[i][j]);
-        document.querySelector('#selectCity').append(option);
+
+  for (let district in optionsObject) {
+    if (district === document.querySelector('#selectRegion').options[document.querySelector('#selectRegion').selectedIndex].value) {
+      for ( let city of optionsObject[district]) {
+        console.log(optionsObject[district]);
+        let option = new Option(city);
+        document.querySelector('#selectCity').insertBefore(option, document.querySelector('#Not_in_the_list'));
       }
     }
   }
 
-
   const isShouldDisplay = document.querySelector('#selectRegion').selectedIndex > 1 ?  true : false;
   if(isShouldDisplay) {
     document.querySelector('#selectCity').style.display = "block";
+    isRegionSelected = true;
   } else {    
     document.querySelector('#selectCity').style.display = "";
+    isRegionSelected = false;
   }
+  changeButtonState();
 });
 
 document.querySelector('#checkbox').addEventListener('change', function(event) {
   if(document.querySelector('#checkbox').checked) {
-    document.querySelector('#fullName').style.display="none";
     document.querySelector("#phoneNumber").style.display="none";
     document.querySelector('#selectRegion').style.display="none";
+    document.querySelector('#selectCity').style.display = "none";
+    isCheckboxChecked = true;
   } else {
-    document.querySelector('#fullName').style.display="block";
     document.querySelector("#phoneNumber").style.display="block";
     document.querySelector('#selectRegion').style.display="block";
+    document.querySelector('#selectCity').style.display = "";
+    isCheckboxChecked = false;
   }
+  changeButtonState();
 });
+}
