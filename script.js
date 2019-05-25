8fd99bcc75b4ecb2bc0c5d8e80321c8313cad0c6
vpolets <vpolets@gmail.com>
2019-05-25 17:59:18 +0300

    remake solution

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 'use strict';
 
-const formBox = document.querySelector('#form');
-const name = document.querySelector('#full-name');
-const number = document.querySelector('#number');
-const regionSelect = document.querySelector('#region');
-const citySelect = document.querySelector('#city');
-const selectBox = document.querySelector('#select-box');
-const privacy = document.querySelector('#privacyCheck');
-const submit = document.querySelector('#submit');
+function main() {
+  const formBox = document.forms['reg-form'];
+  const extra = formBox.querySelector('#extra-info');
+  const regionSelect = formBox['region'];
+  const selectBox = formBox.querySelector('#select-box');
+  const privacy = formBox['privacy-check'];
+  const submit = formBox['submit'];
+  const regions = {
+    Kyiv: true,
+    Center: ['Cherkasy', 'Dnipro', 'Kropyvnytskyi', 'Poltava', 'Vinnytsia', 'Zhytomyr'],
+    North: ['Chernihiv', 'Sumy'],
+    East: ['Donetsk', 'Kharkiv', 'Luhansk'],
+    South: ['Kherson', 'Mykolaiv', 'Odesa', 'Zaporizhzhia'],
+    West: ['Chernivtsi', 'Ivano-Frankivsk', 'Khmelnytskyi', 'Lutsk', 'Lviv', 'Rivne', 'Ternopil', 'Uzhhorod']
+  };
+  const ignoreStrings = ['', 'Kyiv', 'Not in the list'];
 
-const regions = {
-  Kyiv: 'Kyiv',
-  Center: ['Cherkasy', 'Dnipro', 'Kropyvnytskyi', 'Poltava', 'Vinnytsia', 'Zhytomyr'],
-  North: ['Chernihiv', 'Sumy'],
-  East: ['Donetsk', 'Kharkiv', 'Luhansk'],
-  South: ['Kherson', 'Mykolaiv', 'Odesa', 'Zaporizhzhia'],
-  West: ['Chernivtsi', 'Ivano-Frankivsk', 'Khmelnytskyi', 'Lutsk', 'Lviv', 'Rivne', 'Ternopil', 'Uzhhorod']
-};
-
-const ignoreStrings = ['', 'Kyiv', 'Not in the list'];
+  submit.disabled = true;
+  formBox.addEventListener('input', checkInput);
+  addSelectList(Object.keys(regions), regionSelect);
+  regionSelect.addEventListener('change', selectHandle.bind(null, ignoreStrings, regions));
+  privacy.addEventListener('click', privacyHandle.bind(null, extra));
+  selectBox.addEventListener('change', checkInput);
+  formBox.addEventListener('change', validateForm.bind(null, submit));
+}
 
 const isValid = (stringToValidate, regex) => {
   if (regex.test(stringToValidate)) {
@@ -28,7 +34,20 @@ const isValid = (stringToValidate, regex) => {
   return false;
 };
 
-const inputValidation = (input, normalizeString, regex) => {
+const inputValidation = (input, normalizeString, type) => {
+  let regex;
+  switch(type) {
+    case 'full-name': 
+      regex = /^[a-zA-Zа-яА-ЯёЁїЇІі'\-]{2,30} [a-zA-Zа-яА-ЯёЁїЇІі'\-]{2,30} ?([a-zA-Zа-яА-ЯёЁїЇІі'\-]{2,30})$/;
+      break;
+    case 'number':
+      regex = /^(((380)?[0-9]{9})|(0[0-9]{9}))$/;
+      break;
+    default:
+      regex = /\w+/;
+    break;
+  }
+
   input.classList.toggle('input_correct', isValid(normalizeString, regex));
   input.classList.toggle('input_incorrect', !isValid(normalizeString, regex));
 };
@@ -38,28 +57,33 @@ const removeClassIfEmpty = (input) => {
     input.classList.remove('input_correct');
     input.classList.remove('input_incorrect');
   }
-}
+};
 
-name.addEventListener('change', () => {
-  const normalizeName = name.value.trim().split(/\s+/).join(' ');
-  const regex = /^[a-zA-Zа-яА-ЯёЁїЇ'\-]{2,30} [a-zA-Zа-яА-ЯёЁїЇ'\-]{2,30} ?([a-zA-Zа-яА-ЯёЁїЇ'\-]{2,30})$/;
-  
-  inputValidation(name, normalizeName, regex);
-  removeClassIfEmpty(name);
+const normalizeInput = (value, type) => {
+  switch (type) {
+    case 'full-name': 
+      return value.trim().split(/\s+/).join(' ');
+    case 'number':
+      return value.split(/\D/).join('');
+    default: 
+      return value;
+  }
+};
 
-  name.value = normalizeName;
-});
+const checkInput = (event) => {
+  const { target } = event;
+  let { name, value } = event.target;
+  const normalizeString = normalizeInput(value, name);
+  const form = target.closest('form');
 
-number.addEventListener('input', () => {
-  const normalizeNumber = number.value.split(/\D/).join('');
-  number.value = normalizeNumber;
-});
+  if (name === 'number') {
+    target.value = normalizeString;
+  }
 
-number.addEventListener('change', () => {
-  const regex = /^(((380)?[0-9]{9})|(0[0-9]{9}))$/;
-  inputValidation(number, number.value, regex);
-  removeClassIfEmpty(number);
-});
+  inputValidation(target, normalizeString, name);
+  removeClassIfEmpty(target);
+  validateForm(form['submit']);
+};
 
 const addSelectList = (elements, select) => {
   elements.reverse().forEach(itemText => {
@@ -67,50 +91,56 @@ const addSelectList = (elements, select) => {
     option.textContent = itemText;
     select.children[0].after(option);
   })
-}
+};
 
-const clearSelectList = (select) => {
-  const countOfElems = select.children.length;
-  [...select.children].slice(1, countOfElems - 1)
-    .forEach(option => option.remove());
-}
+const createCitySelect = (regionSelect) => {
+  const citySelect = regionSelect.cloneNode();
+  const blankElem = regionSelect.children[0].cloneNode();
+  const lastElem = document.createElement('option');
 
-addSelectList(Object.keys(regions), regionSelect);
+  lastElem.textContent = 'Not in the list';
+  citySelect.append(blankElem, lastElem);
+  citySelect.name = 'city';
+  citySelect.classList.remove('input_correct');
 
-regionSelect.addEventListener('change', () => {
-  citySelect.classList.add('elem_hidden');
-  const region = regionSelect.value;
+  return citySelect;
+};
 
-  if (!ignoreStrings.includes(region)) {
-    clearSelectList(citySelect);
-    addSelectList(regions[region], citySelect);
-    citySelect.classList.remove('elem_hidden');
-  }
-})
-
-privacy.addEventListener('click', () => {
-  const privacyIsChecked = privacy.checked;
-  number.classList.toggle('elem_hidden', privacyIsChecked);
-  selectBox.classList.toggle('elem_hidden', privacyIsChecked);
-})
-
-selectBox.addEventListener('change', event => {
+const selectHandle = (ignoreList, selectList, event) => {
   const { target } = event;
-  if(!target.classList.contains('form__input')) return;
-  inputValidation(target, target.value, /\w+/)
-  removeClassIfEmpty(target);
-})
+  const region = target.value;
+  const selectWrapper = target.parentElement;
+  const selects = selectWrapper.children;
 
-const isFormDataValid = () => {
+  if (selects.length > 1) {
+    selectWrapper.lastElementChild.remove();
+  }
+
+  if (!ignoreList.includes(region)) {
+    const citySelect = createCitySelect(target);
+    addSelectList(selectList[region], citySelect);
+    selectWrapper.append(citySelect);
+  }
+};
+
+const privacyHandle = (extraBlock, event) => {
+  const privacyIsChecked = event.target.checked;
+  extraBlock.classList.toggle('elem_hidden', privacyIsChecked);
+};
+
+const isFormDataValid = (privacy, form) => {
   if (privacy.checked) {
-    return name.classList.contains('input_correct');
+    return form['full-name'].classList.contains('input_correct');
   } else {
-    return [...formBox.querySelectorAll('.form__field')]
-      .filter(input => !input.classList.contains('elem_hidden'))
+    return [...form.querySelectorAll('.form__field')]
       .every(input => input.classList.contains('input_correct'));
   }
 }
 
-formBox.addEventListener('change', () => {
-  submit.disabled = !isFormDataValid();
-})
+const validateForm = (submitButton) => {
+  const form = submitButton.closest('form');
+  const privacySettings = form['privacy-check'];
+  submitButton.disabled = !isFormDataValid(privacySettings, form);
+}
+
+main();
